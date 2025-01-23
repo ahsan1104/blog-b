3e7d9f947a2b7c6631f2a415947035dfd838a0a6
Muhammad Ahsan <154822065+ahsan1104@users.noreply.github.com>
2025-01-23 18:15:01 +0500

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin:'http://localhost:5173/',
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"]
   })
