e409894b2e4982b8df9201de29f226af3005cda6
Muhammad Ahsan <154822065+ahsan1104@users.noreply.github.com>
2025-01-23 20:44:33 +0500

    Update index.js

--- index.js
+++ index.js
@@ -16,13 +16,7 @@ const MONOGO_URL = process.env.MONOG_URI;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin:'http://localhost:5173/',
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"]
-  })
-);
+app.use(cors();
 app.use(
   fileUpload({
     useTempFiles: true,
